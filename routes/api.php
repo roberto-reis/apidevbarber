3e6ba18c51639a318a31ce24f68c51661c5968f8
José Roberto <tekinforroberto@gmail.com>
2021-09-30 15:16:31 -0300

    Feature: Criado o cadastro de User, login e logout

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarberController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +17,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/ping', function() {
     return ['pong' => true];
 });
+
+// Rotas Auth
+Route::get('/401', [AuthController::class, 'anauthorized'])->name('login');
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+Route::post('/auth/create', [AuthController::class, 'create']);
+
+// Rota User
+Route::get('/user', [UserController::class, 'read']);
+Route::post('/user', [UserController::class, 'update']);
+Route::get('/user/favorites', [UserController::class, 'getFavorites']);
+Route::post('/user/favorites', [UserController::class, 'addFavorite']);
+Route::get('/user/appointments', [UserController::class, 'getAppointments']);
+
+// Rotas barbers
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barbers/{id}', [BarberController::class, 'one']);
+Route::post('/barber/{id}/appointment', [BarberController::class, 'setAppointment']);
+
+Route::get('/search', [BarberController::class, 'search']);
+
